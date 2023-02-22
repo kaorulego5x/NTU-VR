a8511cd78b7bcdd77d23e3cb31a71c03fc7ff107
kaorulego5x <kaorulego5x@gmail.com>
2023-02-22 14:52:45 +0800

    added few stuffs

--- Assets/Scripts/HammerSwing.cs
+++ Assets/Scripts/HammerSwing.cs
@@ -2,20 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class monolith_move : MonoBehaviour
+public class HammerSwing : MonoBehaviour
 {
+    // Start is called before the first frame update
     public float speed;
     public float delaySecond;
     private Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
-        offset = transform.position;
+        
     }
 
     // Update is called once per frame
     void Update()
     {  
-        transform.position = new Vector3(5f * Mathf.Sin(speed * Time.time + delaySecond), 0, 0) + offset;
+        transform.rotation = Quaternion.Euler(0f, 0f, 45f * Mathf.Sin(speed * Time.time + delaySecond));
     }
 }
