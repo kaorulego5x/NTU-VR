31e573debfa054623f939a6c9196f29a529d5e2a
sweesingg <pss321@hotmail.com>
2023-02-23 18:08:58 +0800

    updated rubbish
    - to be removed

---
+++ Assets/Scripts/NEW/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5;
+    public Rigidbody rb;
+
+    float horizontalInput; 
+
+    private void FixedUpdate()
+    {
+        Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
+        Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + forwardMove + horizontalMove); 
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+}
